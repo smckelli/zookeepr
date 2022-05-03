e1a126e0e5de4e1cdbf4547e0b8e7cc1520edb35
smckelli <smckelli@gmail.com>
2022-05-03 16:07:27 -0500

    modified port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -40,6 +42,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
